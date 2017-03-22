879d870d34a8c3bd1e0c6ec42d66ccbf28f6b2d2
Felix Lamouroux <felix@iosphere.de>
2017-03-22 15:34:27 +0100

    Clean up

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: help
+
+JS_FILES = index.js
+
+lint-js: $(JS_FILES) ## Check javascript file for linter warnings
+	@mkdir -p linter-logs
+	@set -o pipefail \
+	&& ./node_modules/eslint/bin/eslint.js $(JS_FILES) | tee linter-logs/js.log
+
+lint-js-fix: $(JS_FILES) ## Check javascript file for linter warnings and fix if possible
+	./node_modules/eslint/bin/eslint.js $(JS_FILES) --fix
+
+
+
+
+
+help: ## Prints a help guide
+	@echo "Available tasks:"
+	@grep -E '^[\%a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- index.js
+++ index.js
@@ -1,71 +1,94 @@
 #!/usr/bin/env node
-var argv = require('yargs')
-    .option('language', { alias: 'l', describe: 'Please provide language code that you wish to make the base language.' })
-    .default('src', 'src')
-    .default('rootModule', 'Translation')
-    .option('output')
-    .demand(['language'])
+
+"use strict";
+
+const argv = require("yargs")
+    .option("language", {alias: "l", describe: "Please provide language code that you wish to make the base language."})
+    .default("rootModule", "Translation")
+    .option("yes", {describe: "Reply 'yes' to all automated prompts."})
+    .option("output", {describe: "If set elm-make will be called to compile a localized version of your app which will be placed in the given directory."})
+    .option("src", {default: "src", describe: "The location of your elm code base. Only relevant if output is set."})
+    .option("elmFile", {default: "Main.elm", describe: "File to compiled by elm-make. Only relevant if output is set."})
+    .demand(["language"])
     .argv;
-var fs = require('fs-extra')
-var glob = require("glob");
-var prompt = require('prompt');
-var symlinkOrCopySync = require('symlink-or-copy').sync;
+const fs = require("fs-extra");
+const glob = require("glob");
+const prompt = require("prompt");
+const symlinkOrCopySync = require("symlink-or-copy").sync;
+const path = require("path");
+const currentDir = process.cwd();
+
+// baseDir is the location of the translation root module as referenced in your elm app
+const baseDir = path.join(currentDir, argv.src, argv.rootModule);
 
 // check if language exists
-var languageDir = argv.rootModule + "/" + argv.language
+const languageDir = path.join(currentDir, argv.rootModule, argv.language);
 if (!fs.existsSync(languageDir)) {
-    console.error("Root module not found", languageDir);
+    console.error("Language module not found", languageDir);
     process.exit(1);
 }
 
 // check if base dir exists and delete if needed (after prompt)
-var baseDir = argv.src + "/" + argv.rootModule
 if (fs.existsSync(baseDir)) {
-    prompt.start();
-    console.log('There already is a translation at', baseDir);
-    console.log('Should I continue and replace it with <' + argv.language + '>?');
-    console.log("enter 'y' to continue");
+    if (!argv.yes) {
+        prompt.start();
+        console.log("There already is a translation at", baseDir);
+        console.log("Should I continue and replace it with <" + argv.language + ">?");
+        console.log("enter 'y' to continue");
 
-    prompt.get(['remove'], function (err, result) {
-        if (result.remove !== "y") {
-            console.log("aborting");
-            process.exit(1);
-        }
+        prompt.get(["remove"], function(err, result) {
+            if (result.remove !== "y") {
+                console.log("aborting");
+                process.exit(1);
+            }
 
-        fs.removeSync(baseDir)
+            fs.removeSync(baseDir);
+            main();
+        });
+    } else {
+        fs.removeSync(baseDir);
         main();
-    });
-
+    }
 } else {
     main();
 }
 
-
+/**
+ * main - Copies/symlinks the language into place
+ */
 function main() {
-    console.log("Will create symlink from " + languageDir + " to " + baseDir)
+    console.log("Will create symlink from " + languageDir + " to " + baseDir);
     symlinkOrCopySync(languageDir, baseDir);
 
     // clean out elm-stuff cache
-    glob("elm-stuff/**/user/project/*/"+argv.rootModule+"*", function (er, files) {
+    glob(path.join(currentDir, "/elm-stuff/**/user/project/*/"+argv.rootModule+"*"), function(er, files) {
         files.forEach(function(file) {
             console.log("clearing out cache at", file);
             fs.removeSync(file);
         });
-        build();
+        if (argv.output) {
+            build(argv.output);
+        }
     });
 }
 
-function build() {
-    if (argv.output) {
-        var exec = require('child_process').exec;
-        var cmd = 'elm-make src/Main.elm --yes --output ' + argv.output + '/' + argv.language + '.js';
-        console.log(cmd);
-        exec(cmd, (err, stdout, stderr) => {
-          if (err) {
+
+/**
+ * build - Builds the elm app with the current language.
+ *
+ * @param  {type} outputDir The directory to which the elm build should be written.
+ *                          Files will be named according to the current language
+ */
+function build(outputDir) {
+    let exec = require("child_process").exec;
+    let elmFile = path.join(argv.src, argv.elmFile);
+    let cmd = "elm-make " + elmFile + " --yes --output " + argv.output + "/" + argv.language + ".js";
+    console.log(cmd);
+    exec(cmd, (err, stdout, stderr) => {
+        if(err) {
             console.error(err);
             return;
-          }
-          console.log(stdout);
-        });
-    }
+        }
+        console.log(stdout);
+    });
 }
